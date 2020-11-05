9c70e92bbdbed9ec0c9c99362fa8fea92e87e4d9
Brian Park <briancpark@berkeley.edu>
2020-11-04 17:34:15 -0800

    add config file

--- setup.py
+++ setup.py
@@ -7,6 +7,15 @@ def main():
 	# Use the setup function we imported and set up the modules.
 	# You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
 	# TODO: YOUR CODE HERE
+    module1 = Extension('numc', sources=['num.c', 'matrix.c'], extra_compile_args=CFLAGS, extra_link_args = LDFLAGS)
+    
+    setup(name='numc',
+          version='1.0',
+          description='A Python3 libary for fast matrix operations!',
+          long_description='CS 61C Project 4 on Parallelism', 
+          author='Brian Park and Kaelyn Kim',
+          author_email='briancpark@berkeley.edu',
+          url='https://cs61c.org/fa20/projects/proj4/')
 
 if __name__ == "__main__":
     main()
